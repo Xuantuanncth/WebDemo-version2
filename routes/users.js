0b42da38abe720830bbaf0969fb9089d1eeb351c
XUANTUAN\Xuan Tuan <xuantuanncth1@gmail.com>
2021-08-28 10:59:20 +0700

    Update user.js

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  
+
 });
 
 // router.get('/search',function(req,res,next){
@@ -19,33 +19,37 @@ router.get('/', function(req, res, next) {
 //   res.render('User/index',{user:nameMatcher});
 // });
 
-router.get('/register',function(req,res,next){
-  res.render('Users/register');
+router.get('/test', function(req, res, next) {
+    res.render('Users/register');
+})
+
+router.get('/register', function(req, res, next) {
+    res.render('Users/register');
 });
 
-router.get('/login', function(req,res,next){
-  res.render('Users/login')
+router.get('/login', function(req, res, next) {
+    res.render('Users/login')
 });
 
-router.post('/user/register',function(req,res,next) {
-  console.log(req.body);
-  db.get('users').push(req.body).write();
-  res.redirect('/');
-});
-
-router.post('/user/login',function(req,res,next) {
-  console.log("User login: ",req.body);
-  var uname_req = req.body.username;
-  var pass_req = req.body.password;
-  var nameMatcher = db.get('users').value().filter(function(user){ 
-    return  user.username.indexOf(uname_req) !== -1;
-   });
-  var passMatcher = db.get('users').value().filter(function(user){ 
-    return user.password.indexOf(pass_req) !== -1 ;
-   });
-  if(nameMatcher && passMatcher) {
+router.post('/user/register', function(req, res, next) {
+    console.log(req.body);
+    db.get('users').push(req.body).write();
     res.redirect('/');
-  }
+});
+
+router.post('/user/login', function(req, res, next) {
+    console.log("User login: ", req.body);
+    var uname_req = req.body.username;
+    var pass_req = req.body.password;
+    var nameMatcher = db.get('users').value().filter(function(user) {
+        return user.username.indexOf(uname_req) !== -1;
+    });
+    var passMatcher = db.get('users').value().filter(function(user) {
+        return user.password.indexOf(pass_req) !== -1;
+    });
+    if (nameMatcher && passMatcher) {
+        res.redirect('/');
+    }
 });
 
 module.exports = router;
